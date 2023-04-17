58c9612de2325174d129b80b429757ce4ae0be73
chimedochir <chimedochir2001@gmail.com>
2023-04-17 18:53:38 +0800

    initial commit

---
+++ pages/posts/[id].js
@@ -0,0 +1,47 @@
+import Layout from "../../components/layout";
+import { getAllPostIds, getPostData } from "../../lib/posts";
+import Date from "../../components/date";
+import utilStyles from "../../styles/utils.module.css";
+import Router, { useRouter } from "next/router";
+// Add this import
+import Head from "next/head";
+
+export async function getStaticProps({ params }) {
+  const postData = await getPostData(params.id);
+  return {
+    props: {
+      postData,
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  const paths = getAllPostIds();
+  return {
+    paths,
+    // fallback: true,
+    // fallback: "blocking",
+    fallback: false,
+  };
+}
+
+export default function Post({ postData }) {
+  const router = useRouter();
+  if (router.isFallback) {
+    return <div>Түр хүлээнэ үү.....</div>;
+  }
+  return (
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
+    </Layout>
+  );
+}
